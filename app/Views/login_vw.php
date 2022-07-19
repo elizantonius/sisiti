35f9cf464e083bcae33c12a1a27c01bbf7943ba5
elizantonius <lonecode1777@gmail.com>
2022-07-19 22:46:58 +0700

    login

--- app/Views/login_vw.php
+++ app/Views/login_vw.php
@@ -4,15 +4,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in</title>
+    <title>login</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
 
-    <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
 
-    <link rel="stylesheet" href="../../dist/css/adminlte.min.css?v=3.2.0">
+    <link rel="stylesheet" href="dist/css/adminlte.min.css?v=3.2.0">
     <script nonce="d1480dc4-77ff-42fe-9f90-ecd5dc6a70a8">
         (function(w, d) {
             ! function(a, e, t, r) {
@@ -73,7 +73,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>Admin</b>LTE</a>
+            <a href="login"><b>Cinema 1011</a>
         </div>
 
         <div class="card">
